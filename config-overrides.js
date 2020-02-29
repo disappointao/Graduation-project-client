0c018d6e9a48728ac72435fb7d70428384eb4968
liuchuangao <liuchuangao@tenda.cn>
2020-02-29 11:57:25 +0800

    按需打包和主题配置

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const {override, fixBabelImports,addLessLoader} = require('customize-cra');
+const theme = require('./antd-theme.json');
+module.exports = override(
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: theme,
+    }),
+    fixBabelImports('import', {
+        libraryName: 'antd-mobile',
+        libraryDirectory: 'es',
+        style: true,
+    })
+);
