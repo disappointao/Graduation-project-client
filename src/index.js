14eebcadefacd8e825bfa7b513ad9b41323fc80e
liuchuangao <liuchuangao@tenda.cn>
2020-02-29 11:57:33 +0800

    入口

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'antd-mobile';
+ReactDOM.render(<Button type="primary">开始</Button>, document.getElementById('root'));
